aa4e278c4851f546b8b8269635a3f9dd30c292d2
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-02-03 13:07:03 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,48 +1,7 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AppTabs from './src/routes/AppTabs';
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function HomeStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="HomeStack"
-        component={() => <Text>Home Stack</Text>}
-      />
-    </Stack.Navigator>
-  );
-}
-function MediaStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="MediaStack"
-        component={() => <Text>Media Stack</Text>}
-      />
-    </Stack.Navigator>
-  );
-}
-function MoreStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="MoreStack"
-        component={() => <Text>More Stack</Text>}
-      />
-      <Stack.Screen
-        name="OtherRoute"
-        component={() => <Text>This is the other route</Text>}
-      />
-    </Stack.Navigator>
-  );
-}
 
 function App(): JSX.Element {
   return (
