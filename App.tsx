4d271e32b19e4631fc1767e58f31580385c9445d
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-02-04 14:46:04 +0100

    state management - redux toolkit setup

--- App.tsx
+++ App.tsx
@@ -3,21 +3,31 @@ import { SafeAreaView, StatusBar, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppTabs from './src/routes/AppTabs';
 import SplashScreen from 'react-native-splash-screen';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
 
 function App(): JSX.Element {
   React.useEffect(() => {
-    SplashScreen.hide(); //hides the splash screen on app load.
+    const splashTimeout = setTimeout(() => {
+      SplashScreen.hide(); //hides the splash screen on app load (after 1 second).
+    }, 1000);
+    return () => {
+      clearTimeout(splashTimeout);
+    };
   }, []);
+
   return (
-    <NavigationContainer>
-      <SafeAreaView
-        style={{
-          flex: 1,
-        }}>
-        <StatusBar />
-        <AppTabs />
-      </SafeAreaView>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaView
+          style={{
+            flex: 1,
+          }}>
+          <StatusBar />
+          <AppTabs />
+        </SafeAreaView>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
--- src/routes/Stacks/Home/index.tsx
+++ src/routes/Stacks/Home/index.tsx
@@ -1,7 +1,17 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import { getDayDevotional } from '../../../store/slices/todayDevotional';
 
 const HomeStack = () => {
+  const { todayDevotional } = useAppSelector(state => state.todayDevotional);
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    dispatch(getDayDevotional()).unwrap();
+  }, []);
+  console.log(todayDevotional);
+
   return (
     <View>
       <Text>HomeStack</Text>
